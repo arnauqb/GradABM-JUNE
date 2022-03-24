2f0f35c47d545be9c472e99e93d0e892f8a47925
Arnau Quera-Bofarull <arnauq@protonmail.com>
2022-03-24 19:53:39 +0000

    fixed tests

--- torch_june/model.py
+++ torch_june/model.py
@@ -7,13 +7,15 @@ from torch_june.cuda_utils import get_fraction_gpu_used
 
 
 class TorchJune(torch.nn.Module):
-    def __init__(self, beta_company, beta_leisure, beta_school, beta_household):
+    def __init__(
+        self, log_beta_company, log_beta_leisure, log_beta_school, log_beta_household
+    ):
         super().__init__()
         self.infection_passing = InfectionPassing(
-            beta_company=beta_company,
-            beta_household=beta_household,
-            beta_leisure=beta_leisure,
-            beta_school=beta_school,
+            log_beta_company=log_beta_company,
+            log_beta_household=log_beta_household,
+            log_beta_leisure=log_beta_leisure,
+            log_beta_school=log_beta_school,
         )
         self.infection_updater = InfectionUpdater()
         self.is_infected_sampler = IsInfectedSampler()
