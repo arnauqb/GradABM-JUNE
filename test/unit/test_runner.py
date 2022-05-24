89eb2f589f14e90fd481cb56432c4001690e540b
Arnau Quera-Bofarull <arnauq@protonmail.com>
2022-05-24 15:46:59 +0100

    boost coverage

--- test/unit/test_runner.py
+++ test/unit/test_runner.py
@@ -2,6 +2,8 @@ import pytest
 import yaml
 import torch
 import os
+import numpy as np
+import pandas as pd
 from pathlib import Path
 
 from torch_june.runner import Runner
@@ -9,13 +11,23 @@ from torch_june.paths import default_config_path
 
 
 class TestRunner:
-    @pytest.fixture(name="runner")
-    def make_runner(self):
+    @pytest.fixture(name="data_path")
+    def get_data_path(self):
         data_path = Path(os.path.abspath(__file__)).parent.parent / "data/data.pkl"
+        return data_path
+
+    @pytest.fixture(name="runner")
+    def make_runner(self, data_path):
         with open(default_config_path, "r") as f:
             parameters = yaml.safe_load(f)
         parameters["data_path"] = data_path
         return Runner.from_parameters(parameters)
+
+    def test__read_from_file(self, runner, data_path):
+        file_runner = Runner.from_file()
+        # except data path all params should be equal
+        file_runner.parameters["data_path"] = data_path
+        assert file_runner.parameters == runner.parameters
 
     def test__get_data(self, runner):
         n_agents = runner.data["agent"].id.shape
@@ -55,3 +67,14 @@ class TestRunner:
         assert len(results["cases_by_age_65"]) == 91
         assert len(results["cases_by_age_80"]) == 91
         assert len(results["cases_by_age_100"]) == 91
+
+    def test__save_results(self, runner):
+        with torch.no_grad():
+            results = runner.run()
+        runner.save_results(results)
+        loaded_results = pd.read_csv("./example/results.csv", index_col=0)
+        for key in results:
+            if key == "dates":
+                continue
+            assert (loaded_results[key] == results[key].numpy()).all()
+
