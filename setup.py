e59417ce2fb28c19caf1839ab78b9941961db6c0
Arnau Quera-Bofarull <arnauq@protonmail.com>
2023-06-19 10:13:37 +0100

    moved configs inside

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(join(this_dir, "requirements.txt")) as f:
 
 setup(
     name="grad_june",
-    version="0.1.4",
+    version="0.1.5",
     description="Differentiable implementation of the JUNE model.",
     url="https://github.com/arnauqb/GradABM-JUNE",
     long_description_content_type="text/markdown",
